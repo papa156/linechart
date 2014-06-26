83edef1315cc006b136ea556568023799b291d2e
Parames Voranartpankul <parguitarhero@hotmail.com>
2014-06-26 11:54:34 +0700

    redhawk-helper.htm >>> reorder the option in the list in alphabet order sample-metrics.json >>> reorder the json object in alphabet order metric.js >>> add an object to calculate the dynamically generate the second and third digit after select the first digit

--- src/metric.js
+++ src/metric.js
@@ -1,3 +1,93 @@
+var metricWithTwoOrThreeDigit= {
+	dfsc :{
+		areasearch :"",
+		cassandra:{
+			cityavailablility : 'cityavailablility',
+			decompress : 'decompress',
+			execute : 'execute',
+			fetch : 'fetch',
+			fetchPrepared : 'fetchPrepared',
+			ratesilo : 'ratesilo',
+			timeout: 'timeout'
+		},
+		citysearch:{
+			none: "",
+			filtering: "filtering",
+			gethoteldata: 'gethoteldata',
+			sorting : 'sorting',
+			sortingfiltering: 'sortingfiltering'
+		},
+		converthotelandsteakhouse :"",
+		db : {
+			citymetadata : 'citymetadata',
+			exchangerate : 'exchangerate',
+			hotel_facility : 'hotel_facility',
+			hotelmetadata : 'hotelmetadata',
+			hotelsbycity : 'hotelsbycity',
+			hoteltranslatedname : 'hoteltranslatedname',
+			landmarkdata : 'landmarkdata',
+			manualdownlift : 'manualdownlift',
+			optoutslist : 'optoutslist',
+			partnerfilter : 'partnerfilter',
+			radiusdata : 'radiusdata',
+			review_score : 'review_score',
+			steakhousedata : 'steakhousedata'
+		},
+		dmc : {
+			FetchDmcHotels : "FetchDmcHotels",
+			'request.getcityavailability2' : "request.getcityavailability2",
+			'request.getresults' : "request.getresults",
+			XccFetchHotels : "XccFetchHotels"
+		},
+		getmissingcitydmchotels : "",
+		getsmallhotels : "",
+		hotellist : "",
+		landmarksearch : "",
+		manualcalculation : {
+			none : "",
+			convert : "convert",
+			put : "put"
+		},
+		radiussearch : "",
+		requests : "",
+		steakhouse : {
+			preloadcache : "preloadcache"
+		}
+	},
+	FE : {
+		DragonFruitAPI : "",
+		PagePerformance : {
+			ConnectTime : 'ConnectTime',
+			DnsTime : 'DnsTime',
+			DomCompleteTime : 'DomCompleteTime',
+			DomContentLoadedTime : 'DomContentLoadedTime',
+			DomLoadTime : 'DomLoadTime',
+			DomReadyTime : 'DomReadyTime',
+			LoadEventTime : 'LoadEventTime',
+			RedirectTime : 'RedirectTime',
+			RequestTime : 'RequestTime',
+			ResponseTime : 'ResponseTime'
+		},
+		Performance : {
+			Breakdown : 'Breakdown'
+		},
+		RoundRobinStats : {
+			CDBs : 'CDBs'
+		}
+	},
+	rhtest : {
+		testData : {
+			'14' : 14, 
+			'15' : 15,
+			'19' : 19,
+			'21' : 21,
+			'22' : 22,
+			'23' : 23,
+			'4' : 4
+		}
+	}
+}
+
 function getSelectedItemFromMetricDropDownList(){
 	var metricDropdownList = document.getElementById("metricList");
 	var metricDropdownList2 = document.getElementById("metricList2");
