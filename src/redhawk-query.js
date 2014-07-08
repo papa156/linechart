a2e5406a185d09f8db9ac8ca51b3136c4c00068f
Parames Voranartpankul <parguitarhero@hotmail.com>
2014-07-08 13:48:06 +0700

    change url server

--- src/redhawk-query.js
+++ src/redhawk-query.js
@@ -1,8 +1,10 @@
 var serverUrlList = {
-	"DEV - Development" : "http://as-agdpapi-3001.agprod2.agoda.local:48090/rest/measurements",
-	"HKG - Hong Kong": "http://hk-agdpapi-2001:38090/rest/measurements",
-	"ASH - Ashbourne":"http://as-agdpapi-3001:38090/rest/measurements",
-	"AMS - Amsterdam":"http://am-agdpapi-4001:38090/rest/measurements"
+	"HKG - APP" : "http://hk-agdpapi-2001:38090/rest/measurements",
+	"HKG - DMZ" : "http://hk-agdpapi-2001:38090/rest/measurements",
+	"HKG - DEV" : "http://hk-agdpcc-2001:38090/rest/measurements",
+	"AMS - DMZ":"http://am-agdpapi-4001:38090/rest/measurements",
+	"AMS - APP":"http://am-agdpapi-4001:38090/rest/measurements",
+	"ASH - FE":"http://as-agdpapi-3001:38090/rest/measurements"
 };
 
 var requestObject = {};
